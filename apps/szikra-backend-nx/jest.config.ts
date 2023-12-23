f23a2f28410a8ad792e347ac334caedb00d3942f
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-12-23 22:19:03 +0100

    Initial commit

---
+++ apps/szikra-backend-nx/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'szikra-backend-nx',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/szikra-backend-nx',
+};
